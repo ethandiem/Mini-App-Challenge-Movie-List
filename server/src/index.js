c23fc446e9527844e2c9cc8edcb97d5129f1635b
Ethan <ethanadiem@gmail.com>
2025-03-27 14:01:33 -0600

    level 2 Done

--- server/src/index.js
+++ server/src/index.js
@@ -16,8 +16,19 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.status(200).json({message: "MOOOOOVIES HOME"});
 });
+
+app.get('/movies', async (req, res) => {
+	try {
+			const query = await knex('movies')
+															.select('*')
+			res.status(200).json(query)
+	} catch (error) {
+			console.error('Error fetching units:', error);
+			res.status(500).json({ error: 'Failed to retrieve units' });
+	}
+})
 
 const server = app.listen(PORT, () => {
 	console.log(`App listening at http://localhost:${PORT}`);
